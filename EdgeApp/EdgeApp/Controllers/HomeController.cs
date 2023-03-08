7d11dcb85544d7f284c8c54717e56ca93ee069ae
Marvin Garcia <magar@microsoft.com>
2023-03-08 15:16:01 -0500

    consolidated all content in home page for edge app

--- EdgeApp/EdgeApp/Controllers/HomeController.cs
+++ EdgeApp/EdgeApp/Controllers/HomeController.cs
@@ -6,16 +6,38 @@ namespace EdgeApp.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        // Build version
         ViewData["AppVersion"] = "1.0.0";
+        
+        // Settings
+        Dictionary<string, string> settings = new Dictionary<string, string>()
+        {
+            { "temperature", _configuration["TEMPERATURE"] },
+            { "pressure", _configuration["PRESSURE"] },
+            { "velocity", _configuration["VELOCITY"] },
+        };
+        ViewData["Settings"] = settings;
+        
+        // Secrets
+        Dictionary<string, string> secrets = new Dictionary<string, string>()
+        {
+            { "token", _configuration["TOKEN"] },
+            { "username", _configuration["USERNAME"] },
+            { "password", _configuration["PASSWORD"] },
+        };
+        ViewData["Secrets"] = secrets;
+
         return View();
     }
 
